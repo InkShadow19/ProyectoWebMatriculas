e56af06677b1031ae73d3f0ac22a81b7690264e3
InkShadow19 <SM74726725@idat.edu.pe>
2025-07-19 20:27:10 -0500

    Versión 1.2.7

---
+++ src/app/services/banco.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { BancoDomainService } from '../domains/banco-domain.service';
+import { BancoDto } from '../models/banco.model';
+import { PageResponse } from '../models/page-response.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BancoService implements OnDestroy {
+
+    isLoadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    private unsubscribe: Subscription[] = [];
+
+    constructor(
+        private bancoDomainService: BancoDomainService,
+    ) { }
+
+    getList(page: number = 0, size: number = 10, codigo?: string, descripcion?: string, estado?: string): Observable<PageResponse<BancoDto> | undefined> {
+        this.isLoadingSubject.next(true);
+        // Pasamos tanto el código como la descripción al domain service
+        return this.bancoDomainService.getList(page, size, codigo, descripcion, estado).pipe(
+            catchError(() => of(undefined)),
+            finalize(() => this.isLoadingSubject.next(false))
+        );
+    }
+
+    add(body: Partial<BancoDto>): Observable<BancoDto | undefined> {
+        this.isLoadingSubject.next(true);
+        return this.bancoDomainService.add(body).pipe(
+            catchError(() => of(undefined)),
+            finalize(() => this.isLoadingSubject.next(false))
+        );
+    }
+
+    update(identifier: string, body: Partial<BancoDto>): Observable<BancoDto | undefined> {
+        this.isLoadingSubject.next(true);
+        return this.bancoDomainService.update(identifier, body).pipe(
+            catchError(() => of(undefined)),
+            finalize(() => this.isLoadingSubject.next(false))
+        );
+    }
+
+    delete(identifier: string): Observable<boolean> {
+        this.isLoadingSubject.next(true);
+        return this.bancoDomainService.delete(identifier).pipe(
+            map(() => true),
+            catchError(() => of(false)),
+            finalize(() => this.isLoadingSubject.next(false))
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe.forEach(sub => sub.unsubscribe());
+    }
+}
